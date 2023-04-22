aa6f6e0c1cf883ec948ee5f67ff1d729c8a79e51
Famil Gayibov <familgayibov20@gmail.com>
2023-04-22 03:13:16 +0200

    IDK

---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using BulkyBook.DataAccess.Repository.IRepostory;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
+            return View(objProductList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product obj)
+        {
+            if (!(obj.ListPrice >= obj.Price && obj.Price >= obj.Price50 && obj.Price50 >= obj.Price100))
+            {
+                ModelState.AddModelError("price", "The List price has to be higher or equal to the Price and all concecutive prices has to follow this rule!");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product was created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var productFromDb = _unitOfWork.Product.GetFirstOrDefault(x => x.Id == id);
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(productFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product obj)
+        {
+            if (!(obj.ListPrice >= obj.Price && obj.Price >= obj.Price50 && obj.Price50 >= obj.Price100))
+            {
+                ModelState.AddModelError("price", "The List price has to be higher or equal to the Price and all concecutive prices has to follow this rule!");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product was updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var productFromDb = _unitOfWork.Product.GetFirstOrDefault(x => x.Id == id);
+
+            if(productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.Product.GetFirstOrDefault(x => x.Id == id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Product was deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
